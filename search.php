066679b149e41839aaafc81690d5cedc5de19307
GillFillget <bretm1002@ksu.edu>
2022-10-13 20:43:28 -0500

    added output of select

--- search.php
+++ search.php
@@ -12,4 +12,15 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 $sql = "SELECT * FROM landdeeds;"
+$result = $conn->query($sql);
+
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
 ?> 
