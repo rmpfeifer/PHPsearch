8acd2c637b1801e5887a47c538e5803f598485ae
GillFillget <bretm1002@ksu.edu>
2022-10-13 20:38:00 -0500

    added select and db

--- search.php
+++ search.php
@@ -2,13 +2,14 @@
 $servername = "localhost";
 $username = "phplink";
 $password = "phpsqllink";
+$dbname = "myDB";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+$sql = "SELECT * FROM landdeeds"
 ?> 
