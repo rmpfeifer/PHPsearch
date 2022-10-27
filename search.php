bf862abbed4de31ef022aa7ba4dec4ac26a35f1c
GillFillget <bretm1002@ksu.edu>
2022-10-27 15:19:45 -0500

    Fixed Formatting

--- search.php
+++ search.php
@@ -1,65 +1,59 @@
 <!DOCTYPE html>
-<html>
-<head>
-<style>
-table {
-  width: 100%;
-  border-collapse: collapse;
-}
+  <html>
+    <head>
+      <style>
+      table {
+        width: 100%;
+        border-collapse: collapse;
+      }
 
-table, td, th {
-  border: 1px solid black;
-  padding: 5px;
-}
+      table, td, th {
+        border: 1px solid black;
+        padding: 5px;
+      }
 
-th {text-align: left;}
-</style>
-</head>
-<body>
+      th {text-align: left;}
+      </style>
+    </head>
+  <body>
+    <?php
+      $search = $_GET ['search'];
 
-<?php
+      $servername = "localhost";
+      $username = "phplink";
+      $password = "phpsqllink";
+      $db = "landdeeds";
+      $table = "landdeeds";
 
-$search = $_GET ['search'];
+      $con = mysqli_connect($servername,$username,$password);
+      if (!$con) {
+        die('Could not connect: ' . mysqli_error($con));
+      }
 
-$servername = "localhost";
-$username = "phplink";
-$password = "phpsqllink";
-$db = "landdeeds";
-$table = "landdeeds";
-
-$con = mysqli_connect($servername,$username,$password);
-if (!$con) {
-  die('Could not connect: ' . mysqli_error($con));
-}
-
-mysqli_select_db($con,$db);
-$sql="SELECT * FROM $table WHERE Last_Name_Grantee_1 LIKE '%$search%'";
-$result = mysqli_query($con,$sql);
+      mysqli_select_db($con,$db);
+      $sql="SELECT * FROM $table WHERE Last_Name_Grantee_1 LIKE '%$search%'";
+      $result = mysqli_query($con,$sql);
 
 
-echo "<table>
-<tr>
-<th>Date</th>
-<th>Name</th>
-<th>Township</th>
-<th>Range</th>
-</tr>";
-while($row = mysqli_fetch_array($result)) {
-  echo "<tr>";
-  echo "<td>" . $row['DATE'] . "</td>";
-  echo "<td>" . $row['Last_Name_Grantee_1'] . "</td>";
-  echo "<td>" . $row['TSP'] . "</td>";
-  echo "<td>" . $row['RGE'] . "</td>";
-  echo "</tr>";
-}
+      echo "<table>
+      <tr>
+      <th>Date</th>
+      <th>Name</th>
+      <th>Township</th>
+      <th>Range</th>
+      </tr>";
+      while($row = mysqli_fetch_array($result)) {
+        echo "<tr>";
+        echo "<td>" . $row['DATE'] . "</td>";
+        echo "<td>" . $row['Last_Name_Grantee_1'] . "</td>";
+        echo "<td>" . $row['TSP'] . "</td>";
+        echo "<td>" . $row['RGE'] . "</td>";
+        echo "</tr>";
+      }
 
-
-echo "</table>";
-mysqli_close($con);
-?>
-  <p id="output"></p>
-
-  <a href="form.html">Return</a>
-  <script src="script.js"></script> 
-</body>
+      echo "</table>";
+      mysqli_close($con);
+    ?>
+    <script type="text/javascript" src="script.js"></script>
+  </body>
 </html> 
